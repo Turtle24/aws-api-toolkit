35ffacea0ca8a5b785dc1fdf4590367ac0c7433c
Turtle24 <aidan.nell@yahoo.com>
2021-04-18 15:31:08 +0200

    setup rds connection for client and data api, need arn secret to access db instance data

--- connections/rds.py
+++ connections/rds.py
@@ -4,31 +4,25 @@ import json
 from settings import AWS_ACCESS_KEY_ID, AWS_SECRET_KEY
 
 
-class RedShiftData:
+class RDSClient:
     """ Class that represents the RedShift Data api client connection
     """
-
     def __init__(self):
         self.client = boto3.client(
-                'redshift-data',
+                'rds',
                 aws_access_key_id=AWS_ACCESS_KEY_ID,
                 aws_secret_access_key=AWS_SECRET_KEY,
         )
 
-    def list_tables(self):
+    def account_attributes(self):
         """ A method that list the tables in the redshift database.
 
         Returns:
             list: A list of all the table names is return.
         """
 
-        response = self.client.list_tables(
-            ClusterIdentifier='redshift-cluster-1',
-            Database='dev',
-            DbUser='awsuser',
-            MaxResults=20,
-        )
-        return [dicts['name'] for dicts in response['Tables']]
+        response = self.client.describe_account_attributes()
+        return json.dumps(response, indent=2, default=str)
 
     def describe_tables(self, table):
         """ A method that describes the given table passed to it.
@@ -69,3 +63,27 @@ class RedShiftData:
 #     ClusterIdentifier='redshift-cluster-1',
 #     MaxRecords=20,
 # )
+
+
+class RDSData:
+    def __init__(self):
+        self.client = boto3.client(
+                'rds-data',
+                aws_access_key_id=AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=AWS_SECRET_KEY,
+        )
+
+    def sql_statement(self):
+        response = self.client.execute_statement(
+            continueAfterTimeout=True,
+            database='mysql-db-project',
+            includeResultMetadata=True,
+            resourceArn='arn:aws:rds:us-east-2:937543116659:db:mysql-db-project',
+            resultSetOptions={
+                'decimalReturnType': 'STRING',
+            },
+            schema='Group-Project-Dev',
+            awsSecretStoreArn='secret',
+            sql="SELECT * FROM `Group-Project-Dev`.US_Counties_Covid ORDER BY date ASC limit 1;",
+        )
+        return json.dumps(response, indent=2, default=str)
--- explore.py
+++ explore.py
@@ -1,15 +1,23 @@
-from connections.redshift import RedShiftData
-from connection_settings.redshift_inputs import RedShiftInputs
-from connections.s3 import S3Data
+# from connections.redshift import RedShiftData
+# from connection_settings.redshift_inputs import RedShiftInputs
+from connections.rds import RDSClient, RDSData
+# from connections.s3 import S3Data
 
-from connection_manager.aws_connection_handler import ConnectionHandler
-import json
+# from connection_manager.aws_connection_handler import ConnectionHandler
+# import json
 
-inputs = RedShiftInputs('redshift-cluster-1', 'dev', 'awsuser', 20)
+# inputs = RedShiftInputs('redshift-cluster-1', 'dev', 'awsuser', 20)
 
-tables = RedShiftData().list_tables()
+# tables = RedShiftData().list_tables()
 
-table_description = RedShiftData().describe_tables(tables[0])
+# table_description = RedShiftData().describe_tables(tables[0])
 
-table_columns = [dicts['name'] for dicts in table_description['ColumnList']]
-print(table_columns)
+# # table_columns = [dicts['name'] for dicts in table_description['ColumnList']]
+# print(table_description)
+
+# print(RDSClient().account_attributes())
+
+print(RDSData().sql_statement())
+
+if __name__ == '__main__':
+    print('main')
